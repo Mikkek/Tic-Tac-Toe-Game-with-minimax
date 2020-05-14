9e294211abd7d2b0196445dbf49c8c5bd9c6f5ff
Mikkek <54848327+Mikkek@users.noreply.github.com>
2020-05-14 08:54:38 +0200

    Update NewJFrame.java
    Updated author and removed empty licence comment

--- src/NewJFrame.java
+++ src/NewJFrame.java
@@ -1,16 +1,11 @@
 
 import javax.swing.*;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author ericv
+ * @author Mikkel
  */
+
 public class NewJFrame extends javax.swing.JFrame {
 
     /**
